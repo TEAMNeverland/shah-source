c00c2f2b1448785a8d518ec67e09c9ebab797dba
TEAMNeverland <92953287+TEAMNeverland@users.noreply.github.com>
2025-07-01 10:55:02 +0300

    Delete plugins directory

--- plugins/ranks.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("رفع") & filters.group)
-def promote(client, message):
-    message.reply_text("تم رفع العضو بالرتبة المطلوبة.")
--- plugins/start.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("start"))
-def start_command(client, message):
-    message.reply_text("أهلًا بك في سورس الشاه 👑")
